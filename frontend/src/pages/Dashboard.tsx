c0f6e482ff0962cdb92f176fb084649570b729f2
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2026-01-29 16:15:42 -0300

    feat: dashboard consuming API with loading and error states

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,24 +1,36 @@
 import { useEffect, useState } from "react"
-import { getDashboardStats } from "../api/dashboard"
-import type { DashboardStats } from "../types/Dashboard"
+import api from "../api/axios";
+import type { DashboardData } from "../types/Dashboard";
 
 export default function Dashboard() {
-    const [stats, setStats] = useState<DashboardStats | null>(null);
+    const [data, setData] = useState<DashboardData | null>(null);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
 
     useEffect(() => {
-        getDashboardStats()
-        .then(setStats)
-        .finally(() => setLoading(false));
+        const fetchDashboard = async () => {
+            try {
+                const response = await api.get("/dashboard");
+                setData(response.data)
+            } catch (error) {
+                setError("Failed to load dashboard data")
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchDashboard();
     }, []);
 
     if (loading) {
         return <p className="text-gray-500">Loading dashboard...</p>
     }
 
-    if (!stats) {
-        return <p className="text-red-500">Failed to load dashboard</p>
+    if (error) {
+         return <p className="text-red-500">{error}</p>;
     }
+
+    if (!data) return null;
     
     return (
         <div>
@@ -29,16 +41,16 @@ export default function Dashboard() {
             <div className="grid  grid-cols-1 md:grid-cols-3 gap-6">
                 <div className="bg-white p-6 rounded shadow">
                     <p className="text-gray-500">Total Products</p>
-                    <h3 className="text-3xl font-semibold">{stats.totalProducts}</h3>
+                    <h3 className="text-3xl font-semibold">{data.totalProducts}</h3>
                 </div>
                 
                 <div className="bg-white p-6 rounded shadow">
                     <p className="text-gray-500">Total Sales</p>
-                    <h3 className="text-3xl font-semibold">{stats.totalSales}</h3>
+                    <h3 className="text-3xl font-semibold">{data.totalSales}</h3>
                 </div>
                 <div className="bg-white p-6 rounded shadow">
                     <p className="text-gray-500">Low Stock</p>
-                    <h3 className="text-3xl font-semibold">{stats.lowStock}</h3>
+                    <h3 className="text-3xl font-semibold">{data.lowStock}</h3>
                 </div>
 
             </div>
--- frontend/src/types/Dashboard.ts
+++ frontend/src/types/Dashboard.ts
@@ -1,4 +1,4 @@
-export interface DashboardStats {
+export interface DashboardData {
     totalProducts: number,
     totalSales: number,
     lowStock: number,
