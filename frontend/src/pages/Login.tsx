df60b1b28fe131bb2155ff46f624b15da1f5e777
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2026-01-29 19:22:49 -0300

    feat(products): load products with API integration and loading state

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import api from "../api/axios";
 import { useAuth } from "../auth/AuthContext";
 
@@ -10,17 +11,64 @@ interface LoginForm {
 export default function Login() {
     const { register, handleSubmit } = useForm<LoginForm>();
     const { login } = useAuth();
+    const navigate = useNavigate();
 
     const onSubmit = async (data: LoginForm) => {
         const response = await api.post("/auth/login", data);
         login(response.data);
+
+        navigate("/");
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("email")} placeholder="Email" />
-            <input {...register("password")} type="password" placeholder="Senha" />
-            <button type="submit">Enter</button>
-        </form>
+    <>
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="w-full max-w-md bg-white rounded-xl shadow-lg p-8">
+                <h1 className="text-2xl font-bold text-gray-800 mb-6 text-center">
+                    System Access
+                </h1>
+
+                <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700
+                        mb-1">
+                            Email
+                        </label>
+                        <input 
+                            {...register("email")}
+                            type="email" 
+                            placeholder="Email" 
+                            className="w-full px-4 py-2 border border-gray-300 rounded-md
+                                       focus:outline-none focus:ring-2 focus:ring-blue-500
+                                       focus:border-blue-500"
+                        />
+                    </div>
+
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700 mb-1">
+                            Password
+                        </label>
+                    <input 
+                        {...register("password")} 
+                        type="password" 
+                        placeholder="password" 
+                        className="w-full px-4 py-2 border border-gray-300 rounded-md
+                                    focus:outline-none focus:ring-2 focus:ring-blue-500
+                                    focus:border-blue-500"
+                    />
+                    </div>
+
+                    <button type="submit"
+                            className="w-full bg-blue-600 text-white py-2 rounded-md
+                                       hover:bg-blue-700 hover:cursor-pointer transition font-semibold"
+                    >
+                        Sign In
+                    </button>
+                </form>
+            </div>
+        </div>
+
+    </>
     )
 }
--- frontend/src/pages/Products.tsx
+++ frontend/src/pages/Products.tsx
@@ -1,13 +1,60 @@
-export default function Products() {
-    return (
-        <div>
-            <h2 className="text-2xl font-semibold">
-                Products
-            </h2>
+import { useEffect, useState } from "react"
+import api from "../api/axios"
+import type { Product } from "../types/Product"
 
-            <p  className="text-gray-600 mt-2">
-                Page under construction.
-            </p>
+
+export default function Products() {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadProducts() {
+            try {
+                const response = await api.get("/products");
+                setProducts(response.data);
+            } catch (error) {
+                console.error("Error loading products")
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        loadProducts();
+
+    }, []);
+
+    if (loading) {
+        return <p>Loading products...</p>
+    }
+
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold mb-4">
+                Products
+            </h1>
+
+            <table className="w-full border border-gray-200">
+                <thead className="bg-gray-100">
+                    <tr>
+                        <th className="border p-2 text-left">Name</th>
+                        <th className="border p-2">Category</th>
+                        <th className="border p-2">Price</th>
+                        <th className="border p-2">Stock</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map(product => (
+                        <tr key={product._id}>
+                            <td className="border p-2">{product.name}</td>
+                            <td className="border p-2 text-center">{product.category}</td>
+                            <td className="border p-2 text-center">
+                              R$ {product.price.toFixed(2)}
+                            </td>
+                            <td className="border p-2 text-center">{product.stock}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
