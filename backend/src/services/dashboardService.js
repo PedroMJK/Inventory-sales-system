66bb77fa868d8c3ce87cf40edf7446449da0b5c4
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2026-01-31 18:27:15 -0300

    refactor(dashboard): aling customer naming with domain language

--- backend/src/services/dashboardService.js
+++ backend/src/services/dashboardService.js
@@ -1,18 +1,30 @@
 import Product from "../models/Product.js";
 import Sale from "../models/Sale.js";
+import Customer from "../models/Customer.js";
 
 export const getDashboardStatsService = async () => {
-    const totalProducts = await Product.countDocuments();
+  const totalProducts = await Product.countDocuments();
 
-    const totalSales = await Sale.countDocuments();
+  const totalSales = await Sale.countDocuments();
 
-    const lowStock = await Product.countDocuments({
-        stock: { $lte: 5}
-    });
+  const totalCustomers = await Customer.countDocuments();
 
-    return {
-        totalProducts,
-        totalSales,
-        lowStock
-    };
+  const sales = await Sale.find();
+
+  const totalRevenue = sales.reduce(
+    (sum, sale) => sum + sale.total,
+    0
+  );
+
+  const lowStock = await Product.countDocuments({
+    stock: { $lte: 5 }
+  });
+
+  return {
+    totalProducts,
+    totalSales,
+    totalCustomers,
+    totalRevenue,
+    lowStock,
+  };
 };
