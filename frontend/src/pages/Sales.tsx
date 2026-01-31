0c4b042bc3034d88a7b4d0ea327b31f22fe46f1b
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2026-01-31 17:37:25 -0300

    feat(sales): implement sales page with list, form and API integration

--- frontend/src/pages/Sales.tsx
+++ frontend/src/pages/Sales.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react"
+import api from "../api/axios"
+import type { Sale } from "../types/Sale"
+
+import SaleForm from "../components/sales/SaleForm"
+import SaleList from "../components/sales/SaleList"
+
 export default function Sales() {
+    const [sales, setSales] = useState<Sale[]>([]);
+    const [reload, setReload] = useState(false);
+
+    useEffect(() => {
+        async function loadSales() {
+            const response = await api.get("/sales");
+            setSales(response.data);
+        }
+
+        loadSales();
+    }, [reload]);
+
+
     return (
-        <div>
+        <div className="space-y-6">
             <h2 className="text-2xl font-semibold">
                 Sales
             </h2>
 
-            <p  className="text-gray-600 mt-2">
-                Page under construction.
-            </p>
+            <SaleForm onCreated={() => setReload(!reload)} />
+            <SaleList sales={sales} />
         </div>
     )
 }
