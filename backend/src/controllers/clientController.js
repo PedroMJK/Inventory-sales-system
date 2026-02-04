a7e9b9f78975903ddea58bbb314770522ecab639
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2026-02-04 14:41:42 -0300

    feat(customers): add update and delete customer endpoints

--- backend/src/controllers/clientController.js
+++ backend/src/controllers/clientController.js
@@ -1,6 +1,6 @@
 import Customer from "../models/Customer.js";
 
-
+// CREATE
 export const createClient = async (req, res) => {
   try {
     const customer = await Customer.create({
@@ -14,11 +14,54 @@ export const createClient = async (req, res) => {
   }
 };
 
+// READ ALL
 export const getClients = async (req, res) => {
     try {
-        const customers = await Customer.find();
+        const customers = await Customer.find({ createdBy: req.user._id});
         res.json(customers)
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 };
+
+// UPDATE
+export const updateClient = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const updatedCustomer = await Customer.findOneAndUpdate(
+      {_id: id, createdBy: req.user._id},
+      req.body,
+      { new: true }
+    );
+
+    if (!updatedCustomer) {
+      return res.status(404).json({ message: "Customer not found" });
+    }
+
+    res.json(updatedCustomer);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+// DELETE
+
+export const deleteClient = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedCustomer = await Customer.findByIdAndDelete({
+      _id: id,
+      createdBy: req.user._id,
+    });
+
+    if (!deleteClient) {
+      return res.status(404).json({ message: "Customer not found" });
+    }
+
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
