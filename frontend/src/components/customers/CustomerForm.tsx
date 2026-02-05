02c58b5405357166b2b6b8826b2139b6bf76e718
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2026-02-05 10:58:17 -0300

    chore: remove unused import from CustomerForm

--- frontend/src/components/customers/CustomerForm.tsx
+++ frontend/src/components/customers/CustomerForm.tsx
@@ -1,5 +1,4 @@
 import { useForm } from "react-hook-form";
-import { createCustomer } from "../../services/customerService";
 import type { Customer } from "../../types/Customer";
 import { useEffect } from "react";
 
